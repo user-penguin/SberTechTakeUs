1db61a532b6ec98a2406387d1279efba8ea5558e
pravdin97 <pravdin.ev97@gmail.com>
2018-04-07 03:53:24 +0700

    Класс, описывающий структуру, содержащую интервалы и соответствующие им вероятности

---
+++ src/main/java/logic/AssocInterval.java
@@ -0,0 +1,14 @@
+package logic;
+
+/**
+ * Created by Егор on 31.03.2018.
+ */
+public class AssocInterval {
+    public Interval inter;
+    public double chance;
+    public AssocInterval(Interval _inter, double _chance)
+    {
+        inter = _inter;
+        chance = _chance;
+    }
+}
